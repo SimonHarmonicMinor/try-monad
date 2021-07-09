cb1cd32bbdc0c45f67c00c777f7b7bc7df2dbd40
Semyon Kirekov <semyon@kirekov.com>
2021-07-09 13:34:38 +0700

    build(jest): added 'collectCoverage' attribute to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-undef, quotes, indent */
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/dist/**',
+  ],
 };

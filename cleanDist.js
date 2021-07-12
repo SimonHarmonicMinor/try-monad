a85851a5e33ecf7f48f12f095a15b1e28c64cb97
Semyon Kirekov <semyon@kirekov.com>
2021-07-12 20:22:13 +0700

    fix(build): cleanDist.js deletes 'coverage' directory

--- cleanDist.js
+++ cleanDist.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const fs = require('fs');
-fs.rmdirSync('dist', {recursive: true});
+fs.rmdirSync('dist', { recursive: true });
+fs.rmdirSync('coverage', { recursive: true });
